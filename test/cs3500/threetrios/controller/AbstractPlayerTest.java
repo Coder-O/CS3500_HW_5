99cd20687adfbbf567865521b5a0fe93fe5f6085
Coder-O <56103297+Coder-O@users.noreply.github.com>
2024-11-20 23:50:06 -0500

    Fixing the incompleteness of the last commit.

--- test/cs3500/threetrios/controller/AbstractPlayerTest.java
+++ test/cs3500/threetrios/controller/AbstractPlayerTest.java
@@ -9,16 +9,22 @@ import java.util.List;
  * An abstract class for testng player implementations.
  */
 public class AbstractPlayerTest {
+  protected Appendable appendable;
+  protected PlayerActions player;
   @Test
   public void testUpdatesAllControllers() {
+
+    // Set up
     List<PlayerController> controllers = new ArrayList<>();
 
     for (int i = 0; i < 10; i++) {
-      controllers.add(new );
+      controllers.add(new MockPlayerController(appendable));
     }
 
     for (PlayerController controller : controllers) {
-
+      player.addControllerListener(controller);
     }
+
+
   }
 }
--- test/cs3500/threetrios/controller/TestStrategyPlayer.java
+++ test/cs3500/threetrios/controller/TestStrategyPlayer.java
@@ -6,9 +6,5 @@ import org.junit.Test;
  * A testing class for a strategy player.
  */
 public class TestStrategyPlayer {
-
-
-
-  @Test
-  public void pla
+  
 }
